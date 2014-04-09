0cacbadcd1fe18eba61394aab2f1786b4ac635b4
Benjamin Cohen <benjamin.cohen86@gmail.com>
2014-04-09 16:38:57 -0400

    adds sound to Hadouken animation

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+	
  $('.ryu').mouseenter(function(){
  	$('.ryu-still').hide();
  	$('.ryu-ready').show();
@@ -8,15 +9,26 @@ $(document).ready(function() {
  	$('.ryu-still').show();
  })
  .mousedown(function() {
- 	// play hadouken sound
+ 	playHadouken();
  	$('.ryu-ready').hide();
  	$('.ryu-throwing').show();
- 	$('.hadouken').show();
- 	// show hadouken and animate it to the right of the screen	
+ 	$('.hadouken').finish().show()
+ 	.animate( {'left': '1200px'}, 500, 
+ 	function() {
+ 		$(this).hide();
+ 		$(this).css('left', '526px');
+ 		}
+ 	);	
+
  })
  .mouseup(function() {
  	// ryu goes back to his ready position
  	$('.ryu-throwing').hide();
  	$('.ryu-ready').show();
  });
+ function playHadouken () {
+		$('#hadouken-sound')[0].volume = 0.5;
+		$('#hadouken-sound')[0].load();
+		$('#hadouken-sound')[0].play();
+	}
 });
